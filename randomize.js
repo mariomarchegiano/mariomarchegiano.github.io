7b180b0814215196a3f07dd8719ef3f22d544de2
mariomarchegiano <127764119+mariomarchegiano@users.noreply.github.com>
2023-03-14 19:40:15 +0100

    Quote update

--- randomize.js
+++ randomize.js
@@ -1,6 +1,8 @@
 var phrases = [
-    {text: "The internet is fueled by narcissism, but the mountains won't remember who I am", author: "Peter McKinnon", link: "https://www.youtube.com/watch?v=2Z0lv6qlj0Q"},
-    {text: "Text", author: "Author"}
+		{text: "The internet is fueled by narcissism, but the mountains won't remember who I am", author: "Peter McKinnon", link: "https://www.youtube.com/watch?v=2Z0lv6qlj0Q"},
+		{text: "Keep close to Nature’s heart… and break clear away, once in awhile, and climb a mountain or spend a week in the woods. Wash your spirit clean", author: "John Muir"},
+		{text: "The mountains are calling and I must go", author: "John Muir"},
+		{text: "A very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, long text", author: "Me"}
 ]
 
 var quote = phrases[Math.floor(phrases.length * Math.random())]
@@ -10,7 +12,7 @@ document.getElementsByClassName('quotephrase')[0].innerHTML = '"' + quote.text +
 var author = document.getElementsByClassName('author')[0]
 
 if('link' in quote) {
-    author.href = quote.link
+		author.href = quote.link
 } else {
  author.removeAttribute('href')
 }
